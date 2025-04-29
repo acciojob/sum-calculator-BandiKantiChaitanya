0316e3a19dc56cde95e52008d2b4e898dec3c9bb
Bandi Kanti Chaitanya <bandikantichaitanya2@gmail.com>
2025-04-29 15:59:16 +0530

    updated version

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  
+  
+  let [sum,setSum]=useState(0)
+    function handleSum(e){
+      const val = Number(e.target.value);
+      setSum((prev)=>prev+val)
+    }
+    
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Sum Calculator</h1>
+        <form action="">
+        <input type="number" name="num" id="num" onChange={handleSum} />
+        </form>
+        {
+          <p>Sum:{sum}</p>
+        }
     </div>
   )
 }
